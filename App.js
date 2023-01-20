92dda94d4cd1c6d94bbb194e16f36dfe6489fbc0
CarlosAncapi <115656721+CarlosAncapi@users.noreply.github.com>
2023-01-20 18:35:36 -0300

    despliegue y cierre de modal

--- App.js
+++ App.js
@@ -1,30 +1,52 @@
 import React, { useState, useEffect} from 'react';
-import { StyleSheet, ActivityIndicator, ImageBackground, View, Text} from 'react-native';
+import { StyleSheet, ActivityIndicator, Modal, ImageBackground, View, Text, Button} from 'react-native';
 
 
 export default function App() {
+  const [modal, setModal] = useState(false)
 
   return (
     <View style={styles.container}>
-      <ImageBackground
-          style={styles.photo}
-          source={{ uri: 'http://placekitten.com/g/400/400' }}
-          >
-          <Text>Hola ñau</Text>
-      </ImageBackground> 
+      <Modal
+        animationType='slide'
+        transparent={true}
+        visible={modal}
+      > 
+        <View style={styles.center}>
+          <View style={styles.content}>
+            <Text>Soy un modal</Text>
+            <Button title="cerrar modal" onPress={() => setModal(!modal)}></Button>
+          </View>
+        </View>
+      </Modal>
+      <Text> no soy el modal!</Text>
+      <Text> no soy el modal!</Text>
+      <Text> no soy el modal!</Text>
+      <Text> no soy el modal!</Text>
+      <Text> no soy el modal!</Text>
+      <Button title="Abrir modal" onPress={() => setModal(!modal)}/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  photo: {
-    height: 400,
-    width: 400,
+  content: {
+    backgroundColor: '#eee',
+    flex:1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin:25
+  },
+  center: {
+    flex:1,
+    alignItems: 'stretch',
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0,0,0,0.3)'
   },
   container: {
     flex: 1,
     backgroundColor: '#ffff',
-    alignItems: 'stretch',
+    alignItems: 'center',
     justifyContent: 'center',
     paddingTop: 22,
     
