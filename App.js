9b8de13c5bc32e7e5561fb1eda7b9276c8631e41
CarlosAncapi <115656721+CarlosAncapi@users.noreply.github.com>
2023-01-20 10:48:45 -0300

    scroll con flatlist

--- App.js
+++ App.js
@@ -1,89 +1,58 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Dimensions, Button, 
-  TouchableHighlight, ScrollView } from 'react-native';
-
-//const { width, height} = Dimensions.get('window').width
+import { StyleSheet, Text, View, FlatList} from 'react-native';
 
 export default function App() {
-  const [text, setText] = useState('chanchito feliz')
-  const [submit, setSubmit] = useState('')
   return (
     <View style={styles.container}>
-      <ScrollView style={styles.scrollView}>
-      <Text style={{ fontSize:24}}>Texto: {submit}</Text>
-      <Text style={{ fontSize:24}}>Texto: {submit}</Text>
-      <Text style={{ fontSize:24}}>Texto: {submit}</Text>
-      <Text style={{ fontSize:24}}>Texto: {submit}</Text>
-      <Text style={{ fontSize:24}}>Texto: {submit}</Text>
-      <Text style={{ fontSize:24}}>Texto: {submit}</Text>
-      <Text style={{ fontSize:24}}>Texto: {submit}</Text>
-      <Text style={{ fontSize:24}}>Texto: {submit}</Text>
-      <Text style={{ fontSize:24}}>Texto: {submit}</Text>
-      <Text style={{ fontSize:24}}>Texto: {submit}</Text>
-      <Text style={{ fontSize:24}}>Texto: {submit}</Text>
-      <Text style={{ fontSize:24}}>Texto: {submit}</Text>
-      <Text style={{ fontSize:24}}>Texto: {submit}</Text>
-      <Text style={{ fontSize:24}}>Texto: {submit}</Text>
-      <Text style={{ fontSize:24}}>Texto: {submit}</Text>
-      <Text style={{ fontSize:24}}>Texto: {submit}</Text>
-      <Text style={{ fontSize:24}}>Texto: {submit}</Text>
-      <Text style={{ fontSize:24}}>Texto: {submit}</Text>
-      <Text style={{ fontSize:24}}>Texto: {submit}</Text>
-      <Text style={{ fontSize:24}}>Texto: {submit}</Text>
-      <Text style={{ fontSize:24}}>Texto: {submit}</Text>
-      <Text style={{ fontSize:24}}>Texto: {submit}</Text>
-      <Text style={{ fontSize:24}}>Texto: {submit}</Text>
-      <Text style={{ fontSize:24}}>Texto: {submit}</Text>
-      <Text style={{ fontSize:24}}>Texto: {submit}</Text>
-      <Text style={{ fontSize:24}}>Texto: {submit}</Text>
-      <Text style={{ fontSize:24}}>Texto: {submit}</Text>
-      <Text style={{ fontSize:24}}>Texto: {submit}</Text>
-      <Text style={{ fontSize:24}}>Texto: {submit}</Text>
-      <Text style={{ fontSize:24}}>Texto: {submit}</Text>
-      <Text style={{ fontSize:24}}>Texto: {submit}</Text>
-      <Text style={{ fontSize:24}}>Texto: {submit}</Text>
-      <Text style={{ fontSize:24}}>Texto: {submit}</Text>
-      <Text style={{ fontSize:24}}>Texto: {submit}</Text>
-      <Text style={{ fontSize:24}}>Texto: {submit}</Text>
-      <Text style={{ fontSize:24}}>Texto: {submit}</Text>
-
-      <TextInput 
-        style={styles.input} 
-        placeholder='Escribe acá'
-        onChangeText={t => setText(t)}
-        defaultValue={text}
-        />
-        <TouchableHighlight
-          underlayColor={'#999'}
-          activeOpacity={0.2} 
-          onPress={() =>{
-          setSubmit(text)
-          alert('Texto enviado con éxito!')
-        }}>
-        <Text>Aceptar</Text>
-        </TouchableHighlight>
-        </ScrollView>
+      <FlatList
+        data={[
+          { key: '1', name: 'carlos' },
+          { key: '2', name: 'pedrito' },
+          { key: '3', name: 'juan' },
+          { key: '4', name: 'vale' },
+          { key: '5', name: 'Vicky' },
+          { key: '6', name: 'cancerbero' },
+          { key: '7', name: 'carlos' },
+          { key: '8', name: 'pedrito' },
+          { key: '9', name: 'juan' },
+          { key: '10', name: 'vale' },
+          { key: '11', name: 'Vicky' },
+          { key: '12', name: 'cancerbero' },
+          { key: '13', name: 'carlos' },
+          { key: '14', name: 'pedrito' },
+          { key: '15', name: 'juan' },
+          { key: '16', name: 'vale' },
+          { key: '17', name: 'Vicky' },
+          { key: '18', name: 'cancerbero' },
+          { key: '19', name: 'carlos' },
+          { key: '20', name: 'pedrito' },
+          { key: '21', name: 'juan' },
+          { key: '22', name: 'vale' },
+          { key: '23', name: 'Vicky' },
+          { key: '24', name: 'cancerbero' }
+        ]}
+        renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  input: {
-    height:40,
-    backgroundColor:'white',
-    borderBottomWidth:1,
-    width: '100%',
-    textAlign:'center'
-  },
+
   container: {
     flex: 1,
     backgroundColor: 'white',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
+    paddingTop: 22,
+    
   },
-  scrollView: {
-    width : Dimensions.get('window').width
+  item: {
+    fontSize:22,
+    padding:10,
+    height:50,
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+    textAlign: 'center'
   }
 });
-
-///////
