2b4361056ab5989f21cb74bfa0bd2a9c6777df8f
CarlosAncapi <115656721+CarlosAncapi@users.noreply.github.com>
2023-01-20 09:23:08 -0300

    hola mundo

--- App.js
+++ App.js
@@ -1,19 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Dimensions, Button, TouchableHighlight } from 'react-native';
+
+//const { width, height} = Dimensions.get('window').width
 
 export default function App() {
+  const [text, setText] = useState('chanchito feliz')
+  const [submit, setSubmit] = useState('')
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{color: 'white', fontSize:24}}>Texto: {submit}</Text>
+
+      <TextInput 
+        style={styles.input} 
+        placeholder='Escribe acá'
+        onChangeText={t => setText(t)}
+        defaultValue={text}
+        />
+        <Button onPress={() =>{
+          setSubmit(text)
+          alert('Texto enviado con éxito!')
+        }}title='Aceptar'/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  input: {
+    height:40,
+    backgroundColor:'white',
+    borderBottomWidth:1,
+    width: '100%',
+    textAlign:'center'
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
